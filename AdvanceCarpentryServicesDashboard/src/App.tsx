ce7715178580e0447f030e2c9deaec3526826e0f
Raphael Toledo <rapht1902@gmail.com>
2026-05-21 22:08:15 -0700

    sidebar inclusion

--- AdvanceCarpentryServicesDashboard/src/App.tsx
+++ AdvanceCarpentryServicesDashboard/src/App.tsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 
+import Sidebar from "./components/layout/Sidebar";
+
 import DashboardPage from "./pages/DashboardPage";
 import JobsPage from "./pages/JobsPage";
 import JobDetailPage from "./pages/JobDetailPage";
@@ -7,12 +9,18 @@ import SchedulePage from "./pages/SchedulePage";
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<DashboardPage />} />
-            <Route path="/jobs" element={<JobsPage />} />
-            <Route path="/jobs/:jobId" element={<JobDetailPage />} />
-            <Route path="/schedule" element={<SchedulePage />} />
-        </Routes>
+        <div className="flex min-h-screen bg-gray-50">
+            <Sidebar />
+
+            <main className="flex-1 p-6">
+                <Routes>
+                    <Route path="/" element={<DashboardPage />} />
+                    <Route path="/jobs" element={<JobsPage />} />
+                    <Route path="/jobs/:jobId" element={<JobDetailPage />} />
+                    <Route path="/schedule" element={<SchedulePage />} />
+                </Routes>
+            </main>
+        </div>
     );
 }
 
